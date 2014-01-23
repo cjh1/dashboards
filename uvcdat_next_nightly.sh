cd817104f8a01eeb395b42987ac86d1cc8339708
Chris Harris <chris.harris@kitware.com>
2014-01-23 14:55:02 -0500

    Add script for nightly next

---
+++ uvcdat_next_nightly.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PATH=/home/dash/cmake-build/bin:$PATH
+export DASHTYPE=Nightly
+export DASHROOT=/home/dash/dashboards/mytests
+ctest -VV -S /home/dash/uvcdat-devel/CMake/dashboard/dashboard.cmake,"next;ubuntu;salix.cmake" -VV &> /tmp/uvcdat_next_nightly.log
+
