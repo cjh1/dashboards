165d2bf2ccc9b14b8a1b20aae7fde7e0617ee4c9
Chris Harris <chris.harris@kitware.com>
2014-01-23 17:40:28 -0500

    Continuous next updates

--- uvcdat_next_continuous.sh
+++ uvcdat_next_continuous.sh
@@ -1,4 +1,6 @@
-export DASHTYPE=Experimental
-export DASHROOT=/home/kitware/dashboards/mytests
-ctest -S $DASHROOT/UVCDAT/source/master/UVCDAT-Continuous-x86_64dashboard.cmake,"master;arch;salix.cmake" -VV &> /tmp/uvcdat_continuous.log
+#!/bin/bash
+export PATH=/home/dash/cmake-build/bin:$PATH
+export DASHTYPE=Continuous
+export DASHROOT=/home/dash/dashboards/mytests
+ctest -VV -S /home/dash/uvcdat-devel/CMake/dashboard/dashboard.cmake,"next;ubuntu;salix.cmake" -VV &> /tmp/uvcdat_next_continuous.log
 
